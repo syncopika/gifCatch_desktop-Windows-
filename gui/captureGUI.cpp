7b8aa7bfd25bb3cdd9ec76a74c8883a838d9cbc9
Nicholas Hung <nhung93@outlook.com>
2018-01-09 23:18:59 -0500

    catch some screenshots and maybe make a gif on the desktop

---
+++ gui/captureGUI.cpp
@@ -0,0 +1,678 @@
+/**
+
+using the screen capture code from: https://github.com/ebonwheeler/Win32GrabScreen/blob/master/capture.cpp
+
+idea: take a bunch of screenshots in sequence (with interval as n milliseconds), 
+	  create gif from screenshots
+
+	have a GUI, user inputs 3 args
+	- pre-delay, milliseconds (give the user some time to minimize the gui window if they want a gif that is full screen)
+	- number of frames to collect 
+	- interval between screenshots, in milliseconds (10 <= n <= 1000 ms) cap it at 1000
+
+	
+	maybe give fps option and length of gif, not choose delay and number of frames.
+	
+	-> the mechanism to collect frames is in place 
+	-> now need to collect parameters, such as # of frames, # ms delay, and the area to capture 
+	-> # of frames and # ms delay between frames is pretty much in place 
+	-> need to implement a 2nd full-screen, transparent window to capture area 
+	
+	help!
+	https://stackoverflow.com/questions/2382464/win32-full-screen-and-hiding-taskbar
+	https://stackoverflow.com/questions/1857292/how-do-you-get-the-location-in-x-y-coordinate-pixels-of-a-mouse-click
+	https://stackoverflow.com/questions/3970066/creating-a-transparent-window-in-c-win32
+	https://stackoverflow.com/questions/2886609/how-to-make-multiple-windows-using-win32-api
+	https://msdn.microsoft.com/en-us/library/windows/desktop/ms633504(v=vs.85).aspx
+	https://stackoverflow.com/questions/30668040/specifying-a-window-procedure-for-child-windows
+	https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx  // show/hide window 
+	https://stackoverflow.com/questions/15605655/transparent-win32-window-and-text
+	
+	// dragging rectangle / rubberband 
+	https://stackoverflow.com/questions/13336225/win32-draw-a-dragging-rectangle
+	http://jdearden.gotdns.org/programming_windows_notebook/how_to_use_win32_api_to_draw_a_dragging_rectangle_on_screen_dc.html
+	http://www.cplusplus.com/forum/beginner/11226/
+	https://www.codeproject.com/Articles/1988/Guide-to-WIN-Paint-for-Beginners
+	
+	// creating multiple windows 
+	http://www.cplusplus.com/forum/windows/154449/
+	
+	// set active window 
+	https://msdn.microsoft.com/en-us/library/windows/desktop/ms646311(v=vs.85).aspx
+	
+	// yes/no box, prompt user after area selected 
+	http://forums.codeguru.com/showthread.php?445032-Determine-if-message-box-button-YES-or-NO-was-clicked-(MB_YESNO)
+	https://msdn.microsoft.com/en-us/library/windows/desktop/ms645505(v=vs.85).aspx
+	
+	// maybe use Qt to then select part of image, make gif ? nah, just use win32 for now 
+	// https://forum.qt.io/topic/11912/selecting-a-part-of-image/7
+	// http://www.cplusplus.com/forum/windows/106009/
+	
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+#include <windows.h>
+#include <gdiplus.h>
+#include <memory>
+//#include "resource.h"
+
+// give some identifiers for the GUI numbers 
+#define ID_TITLE_LABEL 99
+
+#define ID_NUMFRAMES_LABEL 100
+#define ID_NUMFRAMES_TEXTBOX 101
+#define ID_ADD_NUMFRAMES 102 
+
+#define ID_DELAY_LABEL 103 
+#define ID_DELAY_TEXTBOX 104
+#define ID_ADD_DELAY 105
+
+#define ID_SELECT_SCREENAREA_BUTTON 106 
+#define ID_START_BUTTON 107
+
+/*****************
+
+	global variables to hold parameters
+
+*****************/
+
+int x1 = 0; // top left x coord 
+int y1 = 0; // top left y coord
+int x2 = GetSystemMetrics(SM_CXSCREEN); // screen bottom right x coord 
+int y2 = GetSystemMetrics(SM_CYSCREEN); // screen bottom right y coord 
+
+
+// for selection window 
+bool bDrag = false;
+bool bDraw = false;
+
+// point structs for keeping track of start and final coordinates
+POINT ptCurr = {0, 0};
+POINT ptNew = {0, 0};
+HGDIOBJ original = NULL;
+
+
+using namespace Gdiplus;
+using namespace std;
+
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
+{
+    UINT num = 0;          // number of image encoders
+    UINT size = 0;         // size of the image encoder array in bytes
+
+    ImageCodecInfo* pImageCodecInfo = NULL;
+
+    GetImageEncodersSize(&num, &size);
+    if(size == 0)
+    {
+        return -1;  // Failure
+    }
+
+    pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+    if(pImageCodecInfo == NULL)
+    {
+        return -1;  // Failure
+    }
+
+    GetImageEncoders(num, size, pImageCodecInfo);
+
+    for(UINT j = 0; j < num; ++j)
+    {
+        if( wcscmp(pImageCodecInfo[j].MimeType, format) == 0 )
+        {
+            *pClsid = pImageCodecInfo[j].Clsid;
+            free(pImageCodecInfo);
+            return j;  // Success
+        }    
+    }
+
+    free(pImageCodecInfo);
+    return -1;  // Failure
+}
+
+void BitmapToJpg(HBITMAP hbmpImage, int width, int height, string filename)
+{
+    Bitmap *p_bmp = Bitmap::FromHBITMAP(hbmpImage, NULL);
+    //Bitmap *p_bmp = new Bitmap(width, height, PixelFormat32bppARGB);
+    
+	// how about pngs?
+    CLSID pngClsid;
+    int result = GetEncoderClsid(L"image/jpeg", &pngClsid);
+    if(result != -1){
+        std::cout << "Encoder succeeded" << std::endl;
+	}else{
+        std::cout << "Encoder failed" << std::endl;
+	}
+	
+	// convert filename to a wstring first
+	wstring fname = wstring(filename.begin(), filename.end());
+	
+	// use .c_str to convert to wchar_t*
+    p_bmp->Save(fname.c_str(), &pngClsid, NULL);
+    delete p_bmp;
+}
+
+bool ScreenCapture(int x, int y, int width, int height, const char *filename)
+{
+	HDC hDc = CreateCompatibleDC(0);
+	HBITMAP hBmp = CreateCompatibleBitmap(GetDC(0), width, height);
+	SelectObject(hDc, hBmp);
+	BitBlt(hDc, 0, 0, width, height, GetDC(0), x, y, SRCCOPY);
+    BitmapToJpg(hBmp, width, height, filename);
+	DeleteObject(hBmp);
+	return true;
+}
+
+string int_to_string(int i){
+	stringstream ss;
+	ss << i;
+	string i_str = ss.str();
+	return i_str;
+}
+
+void getSnapshots(int nImages, int delay){
+    // Initialize GDI+.
+    GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR gdiplusToken;
+    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+	
+	// make a temp directory 
+	string dirName = "temp";
+	if(CreateDirectory(dirName.c_str(), NULL)){
+		// do nothing
+	}else if(ERROR_ALREADY_EXISTS == GetLastError()){
+		// if it exists, empty out the directory
+	}else{
+		// directory couldn't be made
+	}
+	
+	// need to be able to move the images to the temp directory! (or not?) 
+	string name;
+	
+	for(int i = 0; i < nImages; i++){
+		// put all images in temp folder 
+		// notice x1, y1, x2, and y2 are global variables 
+		name = "temp/screen" + int_to_string(i) + ".jpg";
+		ScreenCapture(x1, y1, x2 - x1, y2 - y1, name.c_str());
+		Sleep(delay);
+    }
+	
+    //Shutdown GDI+
+    GdiplusShutdown(gdiplusToken);
+	
+	/* this is hard. for GifWriteFrame, I need to convert the bmp file 
+	   to an array of uint8_t, which I don't know how to do right now. 
+	   I'm also not really sure what an ARGB frame is :/. use a python script 
+	   to generate the gif 
+	
+	// make the gif here! using gif.h 
+	int width = x2 - x1;
+	int height = y2 - y1;
+	GifWriter gifWriter;
+	
+	// initialize gifWriter
+	// call the gif "test" - it'll be in the same directory as the executable 
+	GifBegin(&gifWriter, "test.gif", width, height, delay/10);
+	
+	// pass in frames 
+	string nextFrame; 
+	const uint8_t* img;
+	//GifWriteFrame( GifWriter* writer, const uint8_t* image, uint32_t width, uint32_t height, uint32_t delay, int bitDepth = 8, bool dither = false )
+	for(int i = 0; i < nImages; i++){
+		// the argument "img" is supposed to be an array of uint8_t representing an image
+		// https://stackoverflow.com/questions/29058452/how-to-convert-a-bmp-image-into-byte-array-using-c-program
+		//nextFrame = "temp/screen" + int_to_string(i) + ".jpg";
+
+		GifWriteFrame(gifWriter, img, width, height, (delay/10));
+	}
+	GifEnd(&gifWriter);
+
+	*/
+}
+
+
+
+/******************
+
+DO Win32 GUI STUFF HERE 
+
+*****************/
+// register 2 different windows 
+const char g_szClassName[] = "mainGUI";
+const char g_szClassName2[] = "selectionWindow";
+
+// handler variables for the windows 
+HWND hwnd;
+HWND selectionWindow;
+
+/* 
+
+	the window procedure 
+	this handles the functionality of the window it's attached to 
+	
+*/
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+	
+	switch(msg){
+
+		case WM_COMMAND:
+			/* LOWORD takes the lower 16 bits of wParam => the element clicked on */
+			switch(LOWORD(wParam)){
+
+				case ID_SELECT_SCREENAREA_BUTTON:
+				{
+					//implement me!
+					// make a new window to select the area 
+					// hide this window until selection has been made 
+					int x = GetSystemMetrics(SM_CXSCREEN);
+					int y = GetSystemMetrics(SM_CYSCREEN);
+					selectionWindow = CreateWindowEx(
+											WS_EX_LAYERED,
+											g_szClassName2,
+											"selection",
+											WS_TILEDWINDOW,
+											0, 0, x, y,
+											NULL, NULL, GetModuleHandle(NULL), NULL
+										);
+											
+					// make window transparent
+					SetLayeredWindowAttributes(
+						selectionWindow,
+						0, 
+						(255 * 70) / 100, 
+						LWA_ALPHA
+					);
+					
+					// show window
+					ShowWindow(selectionWindow, SW_MAXIMIZE);
+					UpdateWindow(selectionWindow);
+					
+				}
+				break;
+				case ID_START_BUTTON:
+				{	
+					// implement me!
+					// get the parameters 
+					HWND frames = GetDlgItem(hwnd, ID_NUMFRAMES_TEXTBOX);
+					HWND delay = GetDlgItem(hwnd, ID_DELAY_TEXTBOX);
+					
+					TCHAR numFrames[3];
+					TCHAR timeDelay[5];
+					
+					GetWindowText(frames, numFrames, 3);
+					GetWindowText(delay, timeDelay, 5);
+					
+					int nFrames = atoi(numFrames);
+					int tDelay = atoi(timeDelay);
+					
+					/* for debugging */
+					cout << "num frames: " << int_to_string(nFrames) << endl;
+					cout << "delay: " << int_to_string(tDelay) << endl;
+					
+					// get window parameters 
+					// by default screenshot whole screen
+					getSnapshots(nFrames, tDelay);
+					
+				}
+				break;
+			}
+		break;
+		case WM_CLOSE:
+		{
+			DestroyWindow(hwnd);
+		}
+		break;
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+		}
+		break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+/*
+	
+	window procedure for the selection window 
+	
+*/
+
+LRESULT CALLBACK WndProcSelection(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+
+	switch(msg){
+		case WM_LBUTTONDOWN:
+		{
+			SetCapture(hwnd);
+			bDrag = true; // need button down to initiate mousemove step 
+
+			// log the coordinates 
+			GetCursorPos(&ptCurr);
+			// check out the difference between ScreenToClient vs ClientToScreen 
+			ScreenToClient(hwnd, &ptCurr);
+		}
+		break;
+		
+		case WM_MOUSEMOVE:
+		{
+			if(bDrag){
+			
+				bDraw = true;
+				
+				// log the coordinates 
+				GetCursorPos(&ptNew);
+				
+				// convert coordinates
+				ScreenToClient(hwnd, &ptNew);
+				bDrag = false;
+			
+			}else if(bDraw){
+				
+				HDC hdc = GetDC(hwnd);
+				SelectObject(hdc,GetStockObject(DC_BRUSH));
+				SetDCBrushColor(hdc, RGB(255,0,0)); // set color (red)
+				
+				SetROP2(hdc, R2_NOTXORPEN);
+				
+				// erase old rectangle 
+				Rectangle(hdc, ptCurr.x, ptCurr.y, ptNew.x, ptNew.y);
+				
+				// collect current coordinates, set them as new 
+				GetCursorPos(&ptNew);
+				ScreenToClient(hwnd, &ptNew);
+				
+				// draw new rectangle 
+				Rectangle(hdc, ptCurr.x, ptCurr.y, ptNew.x, ptNew.y);
+				ReleaseDC(hwnd, hdc);
+				
+			}
+		}
+		break;
+		case WM_LBUTTONUP:
+		{
+			if(bDraw){
+				
+				// get coordinates 
+				cout << "start x: " << ptCurr.x << ", start y: " << ptCurr.y << endl;
+				cout << "end x: " << ptNew.x << ", end y: " << ptNew.y << endl;
+				
+				// check if ok with user 
+				int response = MessageBox(hwnd, "Is this selection okay?", "Confirm Selection", MB_YESNOCANCEL);
+				if(response == IDCANCEL){
+					bDrag = false;
+					bDraw = false;
+					ptCurr.x = 0;
+					ptCurr.y = 0;
+					ptNew.x = 0;
+					ptNew.y = 0;
+					ReleaseCapture();
+					DestroyWindow(hwnd);
+					return 0; 
+				}else if(response == IDYES){
+					// done, record new parameters 
+					x1 = ptCurr.x;
+					y1 = ptCurr.y;
+					x2 = ptNew.x;
+					y2 = ptNew.y;
+					
+					// do some correction to take into account the title bar height 
+					if(ptCurr.y > 0){
+						//cout << "height of title bar: " << int_to_string(GetSystemMetrics(SM_CYCAPTION)) << endl;
+						y1 = y1 + GetSystemMetrics(SM_CYCAPTION);
+					}
+					
+					DestroyWindow(hwnd);
+					ReleaseCapture();
+					return 0;
+				}else if(response == IDNO){
+					// reset stuff
+					// need to clear screen also!!
+					bDrag = false;
+					bDraw = false;
+					ptCurr.x = 0;
+					ptCurr.y = 0;
+					ptNew.x = 0;
+					ptNew.y = 0;	
+				}else{
+					// failure to show message box	
+					cout << "error with message box!!" << endl;
+					ReleaseCapture();
+					DestroyWindow(hwnd);
+					return 0;
+				}
+			}
+		}
+		break;
+		
+		case WM_CLOSE:
+		{
+			DestroyWindow(hwnd);
+			return 0;
+		}
+		break;
+		case WM_DESTROY:
+		{
+			DestroyWindow(hwnd);
+			return 0;
+		}
+		break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+
+
+}
+
+/*************
+
+	GUI CODE
+	
+**************/
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+
+	/* console attached for debugging */
+	AllocConsole();
+	freopen( "CON", "w", stdout );
+	
+	WNDCLASSEX wc; // this is the main GUI window 
+	MSG Msg;
+	
+	/* register the window class */
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszMenuName = NULL; //MAKEINTRESOURCE(IDR_MYMENU);
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	
+	WNDCLASSEX wc2;
+	/* register the second window class */
+	wc2.cbSize = sizeof(WNDCLASSEX);
+	wc2.style = 0;
+	wc2.lpfnWndProc = WndProcSelection;
+	wc2.cbClsExtra = 0;
+	wc2.cbWndExtra = 0;
+	wc2.hInstance = hInstance;
+	wc2.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc2.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc2.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc2.lpszMenuName = NULL;
+	wc2.lpszClassName = g_szClassName2;
+	wc2.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	
+	
+	if(!RegisterClassEx(&wc)){
+		cout << "error code: " << GetLastError() << endl;
+		MessageBox(NULL, "window registration failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+	
+	if(!RegisterClassEx(&wc2)){
+		cout << "error code: " << GetLastError() << endl;
+		MessageBox(NULL, "window registration failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+	
+	/* create the window */
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		"gifGetter",
+		WS_TILEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 400, 400,
+		NULL, NULL, hInstance, NULL
+	);
+	
+	if(hwnd == NULL){
+		MessageBox(NULL, "window creation failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+	
+	/* make a title label */
+	CreateWindow(
+		TEXT("STATIC"),
+		TEXT(" gif getter \n nch "),
+		WS_VISIBLE | WS_CHILD | SS_LEFT,
+		10, 10,
+		200, 40,
+		hwnd, /* parent window */
+		(HMENU)ID_TITLE_LABEL,
+		hInstance,
+		NULL
+	);
+	
+	
+	/* make text box for # FRAMES TO COLLECT (HWND textInputPriorityLabel) */
+	CreateWindow(
+		TEXT("STATIC"),
+		TEXT("# frames to collect: "),
+		WS_VISIBLE | WS_CHILD | SS_LEFT,
+		10, 90,
+		130, 20,
+		hwnd, /* parent window */
+		(HMENU)ID_NUMFRAMES_LABEL,
+		hInstance,
+		NULL
+	);
+	/* make text box  ADD NUMBER OF FRAMES TO COLLECT  (HWND textInput)*/
+	CreateWindow(
+		TEXT("edit"),
+		TEXT(""),
+		WS_VISIBLE | WS_CHILD | WS_BORDER,
+		150, 90,  /* x, y coords */
+		80, 20, /* width, height */
+		hwnd,
+		(HMENU)ID_NUMFRAMES_TEXTBOX,
+		hInstance,
+		NULL
+	);
+	/* prepopulate text input */
+	SetDlgItemText(hwnd, ID_NUMFRAMES_TEXTBOX, "15");
+	CreateWindow(
+		TEXT("STATIC"),
+		TEXT("1 <= frames <= 50"),
+		WS_VISIBLE | WS_CHILD | SS_LEFT,
+		240, 90,
+		130, 20,
+		hwnd, /* parent window */
+		NULL,
+		hInstance,
+		NULL
+	);
+	
+	
+	/* make text box LABEL FOR DELAY (HWND textInputPriorityLabel) */
+	CreateWindow(
+		TEXT("STATIC"),
+		TEXT("# ms delay: "),
+		WS_VISIBLE | WS_CHILD | SS_LEFT,
+		10, 120,
+		100, 20,
+		hwnd, /* parent window */
+		(HMENU)ID_DELAY_LABEL,
+		hInstance,
+		NULL
+	);
+	CreateWindow(
+		TEXT("edit"),
+		TEXT(""),
+		WS_VISIBLE | WS_CHILD | WS_BORDER,
+		120, 120,  /* x, y coords */
+		80, 20, /* width, height */
+		hwnd,
+		(HMENU)ID_DELAY_TEXTBOX,
+		hInstance,
+		NULL
+	);
+	/* prepopulate text input */
+	SetDlgItemText(hwnd, ID_DELAY_TEXTBOX, "100");
+	CreateWindow(
+		TEXT("STATIC"),
+		TEXT("10 <= ms <= 1000"),
+		WS_VISIBLE | WS_CHILD | SS_LEFT,
+		210, 120,
+		130, 20,
+		hwnd, /* parent window */
+		NULL,
+		hInstance,
+		NULL
+	);
+	
+	
+	
+	/* button to select area of screen  */
+	CreateWindow(
+		TEXT("button"),
+		TEXT("select area"),
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		150, 220,
+		80, 20, 
+		hwnd,
+		(HMENU)ID_SELECT_SCREENAREA_BUTTON,
+		hInstance,
+		NULL
+	);
+	/* button to start */
+	CreateWindow(
+		TEXT("button"),
+		TEXT("start"),
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+		150, 250,
+		80, 20, 
+		hwnd,
+		(HMENU)ID_START_BUTTON,
+		hInstance,
+		NULL
+	);
+	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	
+	/* message loop */
+	while(GetMessage(&Msg, NULL, 0, 0) > 0){
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+## capture screenshots on your Windows desktop and create a gif!    
+    
+the program in the gui folder lets you select what part of the screen to screenshot.    
+this is still heavily in the works but it has almost at least the minimum functionality.    
+right now the gif-making step is handled by the python script, which requires imageio but is extremely easy to install.    
+    
+things to do:    
+- clean up gui    
+- fix the bugs with the rubber-banding/area selection    
+- understand how gifs work    
+- can I get better quality images?    
+    
+thanks to ebonwheeler (https://github.com/ebonwheeler/Win32GrabScreen) for the screen capture code.    
+    
+current screenshots:    
+![the gui](screenshots/gui1.png "the gui")    
+![selecting an area to screenshot](screenshots/gui2.png "selecting an area to screenshot")    
+    
+feel free to check out my Chrome extension that does pretty much the same thing: https://github.com/syncopika/gifCatch_extension    
